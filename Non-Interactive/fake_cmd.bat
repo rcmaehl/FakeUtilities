1f87a48f1bfbf2948399a6b065dddba834168278
Robert C. Maehl <rcmaehl@aol.com>
2019-06-04 11:52:56 -0400

    Update fake_cmd.bat

--- Non-Interactive/fake_cmd.bat
+++ Non-Interactive/fake_cmd.bat
@@ -3,10 +3,13 @@ title C:\windows\system32\cmd.exe
 rem Remove "rem " from in front of the version of Windows you are using this utility on
 rem Windows 10
 rem echo Microsoft Windows [Version 10.0.10240]
+rem echo Copyright (c) 2015 Microsoft Corporation.  All rights reserved.
 rem Windows 8.1
 rem echo Microsoft Windows [Version 6.3.9600]
+rem echo Copyright (c) 2013 Microsoft Corporation.  All rights reserved.
 rem Windows 8
 rem echo Microsoft Windows [Version 6.2.9200]
+rem echo Copyright (c) 2012 Microsoft Corporation.  All rights reserved.
 rem Windows 7 SP1
 echo Microsoft Windows [Version 6.1.7601]
 echo Copyright (c) 2009 Microsoft Corporation.  All rights reserved.
