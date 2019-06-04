1d01c135d0c41336de1ebb3af41ccf8305dc8eba
Robert C. Maehl <rcmaehl@aol.com>
2019-06-04 09:53:39 -0400

    Add fake_powershell.bat

---
+++ Non-Interactive/fake_powershell.bat
@@ -0,0 +1,29 @@
+@echo off
+title C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+echo Windows Powershell
+echo Copyright (c) 2009 Microsoft Corporation.  All rights reserved.
+echo.
+echo PS C:\^>Test Command
+echo Expected output
+echo.
+echo PS C:\^>
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
