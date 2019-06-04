961b85a3b100900de4307cd00f0ae962fcf5b7cb
Robert C. Maehl <rcmaehl@aol.com>
2019-06-04 09:53:04 -0400

    Create fake_cmd.bat

---
+++ Non-Interactive/fake_cmd.bat
@@ -0,0 +1,29 @@
+@echo off
+title C:\windows\system32\cmd.exe
+echo Microsoft Windows [Version 6.1.7601]
+echo Copyright (c) 2009 Microsoft Corporation.  All rights reserved.
+echo.
+echo C:\^>Test command
+echo Expected output
+echo.
+echo C:\^>
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
